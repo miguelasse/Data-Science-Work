059dadedce6586c6e2be8b283e2dab6e3c3fdd46
gatormig <cubang8r@gmail.com>
2020-09-13 14:09:54 -0400

    Initial commit of Tennis Scraper Project

---
+++ TennisScraper/tennis_scraper.py
@@ -0,0 +1,105 @@
+from bs4 import BeautifulSoup
+import html5lib
+import json
+import lxml
+import pandas as pd
+import selenium
+import re
+import requests
+import time
+from tqdm import tqdm
+import yaml
+from utilities import find_utr_ids
+from utilities import find_utr_rating
+from utilities import find_utr_rating_by_name
+
+with open(r'config.yml') as file:
+    config = yaml.safe_load(file)
+
+ids = []
+tennis_recruiting_url = config["tennis_recruiting"]["list_url"]
+boys_18_id = config["tennis_recruiting"]["boys_18_id"]
+
+page = requests.get(tennis_recruiting_url + boys_18_id)
+soup = BeautifulSoup(page.content, "lxml")
+table_body = soup.find_all("tbody")
+
+data = pd.read_html("<table>" + str(table_body) + "/<table>")
+tennis = data[0][:100]
+header = {0:"Rank", 1: "Prev_Rank", 2: "Name", 3: "State", 4: "Stars", 5:"Num_Tournaments"}
+tennis = tennis.rename(mapper=header, axis=1)
+tennis.drop("Stars", axis=1, inplace=True)
+
+tr_player_ids = []
+for row in table_body:    
+    cols = row.find_all("a")
+    for col in cols:
+        id = re.findall("[\d]{6,6}", str(col))
+        if len(id) >= 1:
+            tr_player_ids.append(int(id[0]))
+
+tr_player_ids = set(tr_player_ids)
+tr_player_ids = list(tr_player_ids)
+ 
+tr_player_url = config["tennis_recruiting"]["player_url"]
+
+potential_utr_ids = []
+for id in tqdm(tr_player_ids[0:1]):
+    page = requests.get(tr_player_url + str(id))
+    soup = BeautifulSoup(page.content, "lxml")
+    potential_utr_ids.append(find_utr_ids(soup, "script"))
+
+tr_df = pd.DataFrame(potential_utr_ids)
+header = {0:"tennis_recruiting_id", 1: "utr_id"}
+tr_df = tr_df.rename(mapper=header, axis=1)
+
+tennis = pd.merge(tennis, tr_df, left_index=True, right_index=True)
+header = {0:"player_id"}
+tennis = tennis.rename(mapper=header,axis=1)
+
+utr_login = config["utr"]["login"]
+utr_home = config["utr"]["home"]
+utr_profile = config["utr"]["profile_url"]
+utr_username = config["utr"]["username"]
+utr_password = config["utr"]["password"]
+
+utr_rating = []
+for utr_id in tqdm(tennis["utr_id"]):
+    utr_rating.append(find_utr_rating(utr_login, utr_profile, utr_id, utr_username, utr_password))
+
+utr_rating_df = pd.DataFrame(utr_rating, columns=["utr_id", "utr_rating"])
+
+itr_url = config["itf"]["api_url"]
+
+cookies_part_1 = str(input("Enter in cookies part 1:"))
+cookies_part_2 = str(input("Enter in cookies part 2:"))
+
+page = requests.get(itr_url, cookies={cookies_part_1:cookies_part_2})
+soup = BeautifulSoup(page.content, "lxml")
+site_json=json.loads(soup.text)
+
+itf_df = pd.DataFrame.from_dict(site_json["items"])
+itf_df["playerName"] = itf_df["playerGivenName"] + " " + itf_df["playerFamilyName"]
+
+itf_utr_rating = []
+for name in tqdm(itf_df["playerName"]):
+    itf_utr_rating.append(find_utr_rating_by_name(utr_login, utr_home, name, utr_username, utr_password))
+itf_utr_rating_df = pd.DataFrame(itf_utr_rating, columns=["player_name", "utr_id"])
+
+itf_utr_rating_player_id = []
+for utr_id in tqdm(itf_utr_rating_df["utr_id"]):
+    itf_utr_rating_player_id.append(find_utr_rating(utr_login, utr_profile, utr_id, utr_username, utr_password))
+
+itf_utr_rating_df = pd.DataFrame(itf_utr_rating_player_id, columns=["utr_id", "utr_rating"])
+
+print(itf_utr_rating_df)
+itf_df_final = pd.merge(itf_df, itf_utr_rating_df, left_index=True, right_index=True)
+
+itf_df_final.to_csv("itf_df_final.csv", index=False)
+tennis.to_csv("tennis_recruiting_final.csv", index=False)
+# itf_cols = list(itf_df.columns)
+# ['playerId', 'playerFamilyName', 'playerGivenName', 'playerNationalityCode', 'playerNationality', 
+# 'profileLink', 'hiddenPlayer', 'birthYear', 'rankMovement', 'rank', 'rankEqualFlag', 'tournamentsPlayed', 
+# 'points', 'profileImage', 'tournamentsPlayedSingles', 'tournamentsPlayedDoubles', 'pointsRankingSingles', 
+# 'pointsRankingDoubles', 'pointsRankingTotal']
+
---
+++ TennisScraper/utilities.py
@@ -0,0 +1,149 @@
+def find_utr_ids(soup_data, data_type):
+    import re
+    """
+    Returns the player id and the UTR (Universal Tennis Ranking)
+    ranking of a tennis player.
+
+    Args:
+        soup_data (obj): A beautiful soup object.
+        data_type (str): The string to search for in the beautiful soup object.
+
+    Returns:
+        player_id: An integer of the associated player id
+        utr_id: A float of the Universal Tennis Ranking (UTR)
+    """
+    try:
+        data = soup_data.find_all(data_type)
+        potential_utr_ids = []
+        for item in data:
+            id = re.findall('id":[\d+]+', str(item))
+            if len(id) > 0:
+                potential_utr_ids.append(id)
+                player_id = int(potential_utr_ids[0][0].split(":")[1])
+                utr_id = int(potential_utr_ids[0][2].split(":")[1])
+                        
+        return player_id, utr_id
+    except:
+        return "error", "error"
+
+
+
+def find_utr_rating(login_url, url, utr_id, utr_username, utr_password):
+    from bs4 import BeautifulSoup
+    import html5lib
+    import selenium
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    import re
+    import time
+    """
+    Returns a dictionary of player ids and the UTR (Universal Tennis Ranking)
+    ranking of the player.
+
+    Args:
+        login_url (str): The login URL of the UTR tennis page.
+        url (str): The base URL of the UTR website to pull the rating
+                   from.
+        utr_id (int): The UTR id of the player to concatenate with the
+                      url passed
+
+    Returns:
+        The player id and associated UTR id
+        example: (1234: 5678).
+    """
+    # try:
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    driver.get(login_url)
+
+    time.sleep(1)
+    username = driver.find_element_by_id("emailInput")
+    password = driver.find_element_by_id("passwordInput")
+    username.send_keys(utr_username)
+    password.send_keys(utr_password)
+    driver.find_element_by_xpath("//*[@id='myutr-app-body']/div/div/div/div/div[1]/form/div[3]/button").click()
+    time.sleep(5)
+
+    driver.get(url + str(utr_id))
+    time.sleep(5)
+    html = driver.page_source
+    driver.close()
+
+    page = BeautifulSoup(html, "html5lib")
+    item = page.find_all(class_=re.compile("ratingCircle__utrValue__L_LHa"))
+    for id in item:
+        rating_digit = re.findall(r"(\d+)", str(id))[0]
+        rating_decimal = re.findall(r"(.\d+)", str(id))[2]
+        rating = rating_digit + rating_decimal
+        time.sleep(1)
+    return utr_id, rating
+    
+    # except:
+    #     return utr_id, "error"
+    #     driver.close()
+
+def find_utr_rating_by_name(login_url, url, player_name, utr_username, utr_password):
+    import selenium
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.common.exceptions import NoSuchElementException
+    import re
+    import time
+    """
+    Returns a dictionary of player ids and the UTR (Universal Tennis
+    Ranking) ranking of the player.
+
+    Args:
+        soup_data (obj): A beautiful soup object.
+        data_type (str): The string to search for in the beautiful soup
+                         object.
+
+    Returns:
+        The player name and associated UTR id example: (1234: 5678).
+    """
+    options = Options()
+    #options.add_argument("--headless")
+    #driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
+    driver.get(login_url)
+
+    time.sleep(5)
+    try:
+        username = driver.find_element_by_id("emailInput")
+        password = driver.find_element_by_id("passwordInput")
+        username.send_keys(utr_username)
+        password.send_keys(utr_password)
+        driver.find_element_by_xpath("//*[@id='myutr-app-body']/div/div/div/div/div[1]/form/div[3]/button").click()
+        time.sleep(7)
+        #driver.get(url)
+        #driver.implicitly_wait(3)
+        search = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/nav/div[1]/div[2]/div/div[1]/div/input")
+        time.sleep(5)
+        search.click()
+        driver.implicitly_wait(3)
+        search.send_keys(player_name)
+        driver.implicitly_wait(3)
+    except NoSuchElementException:
+        return "error on login"
+        driver.close()
+    except UnboundLocalError:
+        return "UnboundLocalError"
+        driver.close()
+
+    try:
+        name = driver.find_element_by_class_name("globalSearch__name__3LzQY")
+        name.click()
+        driver.implicitly_wait(2)
+        utr_url = driver.current_url
+        utr_id = utr_url.split("/")[4]
+        time.sleep(1)
+        driver.close()
+        return player_name, utr_id
+    except NoSuchElementException:
+        return player_name, "No UTR id found"
+        driver.close()
+    except UnboundLocalError:
+        return "UnboundLocalError"
+        driver.close()
+    
