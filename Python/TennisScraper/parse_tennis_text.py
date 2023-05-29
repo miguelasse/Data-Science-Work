aa6c0e85074998be89a3582d1b1f7670909b4277
Miguel Asse <10654407+gatormig@users.noreply.github.com>
2023-05-29 16:20:32 -0400

    Update parse_tennis_text.py and utilities functions

--- Python/TennisScraper/parse_tennis_text.py
+++ Python/TennisScraper/parse_tennis_text.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import html5lib
 import json
 import lxml
-import pandas as pd 
+import pandas as pd
 import selenium
 import re
 import requests
@@ -52,21 +52,17 @@ utr_password = config["utr"]["password"]
 
 itf_utr_rating = []
 for name in tqdm(df["player_name"]):
-    itf_utr_rating.append(find_utr_rating_by_name(utr_login, utr_home, name, utr_username, utr_password))
+    itf_utr_rating.append(find_utr_rating_by_name(
+        utr_login, utr_home, name, utr_username, utr_password))
 itf_utr_rating_df = pd.DataFrame(itf_utr_rating)
-itf_utr_rating_df_final = itf_utr_rating_df.iloc[:, 0:2]
+itf_utr_rating_df_final = itf_utr_rating_df.iloc[:, 0:3]
 
-header = {0:"player_name", 1: "utr_id"}
+header = {0: "player_name", 1: "utr_id", 2: "rating"}
 itf_utr_rating_df_final = itf_utr_rating_df_final.rename(mapper=header, axis=1)
 print(itf_utr_rating_df_final)
 
-itf_utr_rating_player_id = []
-for utr_id in tqdm(itf_utr_rating_df_final["utr_id"]):
-    itf_utr_rating_player_id.append(find_utr_rating(utr_login, utr_profile, utr_id, utr_username, utr_password))
-
-itf_utr_rating_player_id_df = pd.DataFrame(itf_utr_rating_player_id, columns=["utr_id", "utr_rating"])
-print(itf_utr_rating_player_id_df)
-itf_df_final = pd.merge(df, itf_utr_rating_player_id_df, left_index=True, right_index=True)
+itf_df_final = pd.merge(df, itf_utr_rating_df_final,
+                        left_index=True, right_index=True)
 print(itf_df_final)
 
 itf_df_final.to_csv("itf_df_final.csv", index=False)
--- Python/TennisScraper/utilities.py
+++ Python/TennisScraper/utilities.py
@@ -21,11 +21,10 @@ def find_utr_ids(soup_data, data_type):
                 potential_utr_ids.append(id)
                 player_id = int(potential_utr_ids[0][0].split(":")[1])
                 utr_id = int(potential_utr_ids[0][2].split(":")[1])
-                        
+
         return player_id, utr_id
     except:
         return "error", "error"
-
 
 
 def find_utr_rating(login_url, url, utr_id, utr_username, utr_password):
@@ -34,6 +33,7 @@ def find_utr_rating(login_url, url, utr_id, utr_username, utr_password):
     import selenium
     from selenium import webdriver
     from selenium.webdriver.firefox.options import Options
+    from selenium.webdriver.common.keys import Keys
     import re
     import time
     """
@@ -69,30 +69,35 @@ def find_utr_rating(login_url, url, utr_id, utr_username, utr_password):
         password = driver.find_element_by_id("passwordInput")
         username.send_keys(utr_username)
         password.send_keys(utr_password)
-        driver.find_element_by_xpath("//*[@id='myutr-app-body']/div/div/div/div/div[1]/form/div[3]/button").click()
+        sign_in = driver.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div[5]/div/div/div/div/div/div[2]/form/div[3]/button")
+        sign_in.send_keys(Keys.RETURN)
         time.sleep(5)
 
         driver.get(url + str(utr_id))
         time.sleep(5)
         html = driver.page_source
+
         driver.close()
 
         page = BeautifulSoup(html, "html5lib")
-        item = page.find_all(class_=re.compile("ratingCircle__utrValue__L_LHa"))
+        item = page.find_all(class_=re.compile(
+            "ratingDisplayV3__value__JpVkj"))
         for id in item:
-            rating_digit = re.findall(r"(\d+)", str(id))[0]
+            rating_digit = re.findall(r"(\d+)", str(id))[1]
             rating_decimal = re.findall(r"(.\d+)", str(id))[2]
             rating = rating_digit + rating_decimal
             time.sleep(1)
-            print(rating)
-        return utr_id, rating
+            return utr_id, rating
         driver.close()
-    
+
     except:
         return utr_id, "error"
         driver.close()
 
+
 def find_utr_rating_by_name(login_url, url, player_name, utr_username, utr_password):
+    from bs4 import BeautifulSoup
     import selenium
     from selenium import webdriver
     from selenium.webdriver.firefox.options import Options
@@ -100,6 +105,8 @@ def find_utr_rating_by_name(login_url, url, player_name, utr_username, utr_passw
     from selenium.common.exceptions import StaleElementReferenceException
     from selenium.common.exceptions import TimeoutException
     from selenium.common.exceptions import WebDriverException
+    from selenium.common.exceptions import InvalidSessionIdException
+    from selenium.webdriver.common.keys import Keys
     import re
     import time
     """
@@ -115,29 +122,45 @@ def find_utr_rating_by_name(login_url, url, player_name, utr_username, utr_passw
         The player name and associated UTR id example: (1234: 5678).
     """
     options = Options()
-    #options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     # options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
-    #driver = webdriver.Firefox()
+    # driver = webdriver.Firefox()
     driver.get(login_url)
 
     time.sleep(5)
     try:
+        # driver.find_element_by_xpath(
+        #     "//*[@id=\"emailInput\"]").click().send_keys(utr_username)
+        # driver.send_keys(utr_username)
+        # driver.find_element_by_xpath(
+        #     "//*[@id=\"passwordInput\"]").click().send_keys(utr_password)
         username = driver.find_element_by_id("emailInput")
         password = driver.find_element_by_id("passwordInput")
         username.send_keys(utr_username)
         password.send_keys(utr_password)
-        driver.find_element_by_xpath("//*[@id='myutr-app-body']/div/div/div/div/div[1]/form/div[3]/button").click()
-        time.sleep(7)
-        #driver.get(url)
-        #driver.implicitly_wait(3)
-        search = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[3]/nav/div[1]/div[2]/div/div[1]/div[1]/input")
+        time.sleep(3)
+        sign_in = driver.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div[5]/div/div/div/div/div/div[2]/form/div[3]/button")
+        sign_in.send_keys(Keys.RETURN)
+        # driver.find_element_by_xpath(
+        #     "//*[@id='myutr-app-body']/div/div/div/div/div[1]/form/div[3]/button").click()
+        time.sleep(5)
+        # driver.get(url)
+        # driver.implicitly_wait(3)
+        search = driver.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div[3]/nav/div[1]/div[2]/div/div[1]/div[1]/input")
         time.sleep(5)
         search.click()
         driver.implicitly_wait(3)
         search.send_keys(player_name)
         driver.implicitly_wait(3)
+
+    except InvalidSessionIdException:
+        driver.stop_client()
+        driver.close()
+        return "error on search"
     except NoSuchElementException:
         driver.stop_client()
         driver.close()
@@ -155,22 +178,59 @@ def find_utr_rating_by_name(login_url, url, player_name, utr_username, utr_passw
         driver.close()
         return "TimeoutException"
     except WebDriverException:
+        driver.stop_client()
+        driver.close()
         return "WebDriverException"
 
     try:
         name = driver.find_element_by_class_name("globalSearch__name__3LzQY")
         name.click()
-        driver.implicitly_wait(2)
-        utr_url = driver.current_url
-        utr_id = utr_url.split("/")[4]
-        time.sleep(1)
+        driver.implicitly_wait(4)
+    except InvalidSessionIdException:
         driver.stop_client()
         driver.close()
-        return player_name, utr_id
+        return "error on search"
     except NoSuchElementException:
         driver.stop_client()
         driver.close()
-        return player_name, "No UTR id found"
+        return "error on search"
+    try:
+        utr_url = driver.current_url
+        utr_id = utr_url.split("/")[4]
+        if utr_url.split("/")[3] == 'clubs':
+            driver.stop_client()
+            driver.close()
+            return player_name, "No UTR id found", "no rating"
+        time.sleep(3)
+        html = driver.page_source
+        page = BeautifulSoup(html, "html5lib")
+        item = page.find_all(class_=re.compile(
+            "ratingDisplayV3__value__JpVkj"))
+    except InvalidSessionIdException:
+        driver.stop_client()
+        driver.close()
+        return "error on search"
+    try:
+        for id in item:
+            try:
+                rating_digit = re.findall(r"(\d+)", str(id))[1]
+                rating_decimal = re.findall(r"(.\d+)", str(id))[2]
+                rating = rating_digit + rating_decimal
+                driver.stop_client()
+                driver.close()
+                return player_name, utr_id, rating
+            except IndexError:
+                driver.stop_client()
+                driver.close()
+                return player_name, utr_id, "no rating"
+    except UnboundLocalError:
+        driver.stop_client()
+        driver.close()
+        return "UnboundLocalError"
+    except NoSuchElementException:
+        driver.stop_client()
+        driver.close()
+        return player_name, "No UTR id found", "no rating"
     except UnboundLocalError:
         driver.stop_client()
         driver.close()
@@ -184,5 +244,6 @@ def find_utr_rating_by_name(login_url, url, player_name, utr_username, utr_passw
         driver.close()
         return "TimeoutException"
     except WebDriverException:
+        driver.stop_client()
+        driver.close()
         return "WebDriverException"
-
